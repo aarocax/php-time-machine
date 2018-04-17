3c3fe37636bf07e862f07c250eff8c7478e03e45
aarocax <aarocax@hotmail.com>
2018-04-17 21:30:27 +0200

    inital

---
+++ index.php
@@ -0,0 +1,86 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+use TimeMachine\src\TimeMachine;
+
+
+$date = new \DateTime('2017-04-25 12:15:00');
+
+$var = TimeMachine::getDateTime($date, 'Europe/Madrid');
+
+var_dump($var);
+
+$timezone = 'Europe/Madid';
+
+if (in_array($timezone , timezone_identifiers_list())) {
+	$timezone = new \DateTimeZone($timezone);
+	echo "sí";
+} else {
+	echo "ní";
+	$timezone = new \DateTimeZone(date_default_timezone_get());
+}
+
+echo "<br>";
+$date = new \DateTime('now', $timezone);
+var_dump($date);
+
+
+//$var = TimeMachine::arrayIntervaloHoras('10:50', '12:30', '30');
+
+
+
+// echo "<br>";
+// $timezone = new DateTimeZone('Europe/London');
+// $var = new DateTime('now', $timezone); //datetime object is on New York time
+// var_dump($var);
+
+// echo "<br>";
+// $timezone = new DateTimeZone('America/New_York');
+// $var = new DateTime('now', $timezone); //datetime object is on New York time
+// var_dump($var);
+
+// echo "<br>";
+// $timezone = new DateTimeZone('Europe/Madrid');
+// $var = new DateTime('now', $timezone); //datetime object is on New York time
+// var_dump($var);
+
+
+
+// $var = new DateTime('now', $timezone); //datetime object is on New York time
+
+
+
+
+
+
+// echo "<br>";
+// $a1 = timezone_identifiers_list();
+// //var_dump($a1);
+// foreach ($a1 as $key => $value) {
+// 	echo $value."<br>";
+// }
+
+
+// //var_dump(createTZList2());
+
+// foreach(timezone_abbreviations_list() as $abbr => $timezone){
+//         foreach($timezone as $val){
+//                 if(isset($val['timezone_id'])){
+//                         var_dump($abbr,$val['timezone_id']);
+//                         echo "<br>";
+//                 }
+//         }
+// }
+
+
+// function createTZList2() {
+//   $out = array();
+//   $tza = timezone_abbreviations_list();
+//   foreach ($tza as $zone)
+//     foreach ($zone as $item)
+//       $out[$item['timezone_id']] = 1;
+//   unset($out['']);
+//   ksort($out);
+//   return array_keys($out);
+// }
---
+++ src/TimeMachine.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace TimeMachine\src;
+
+class TimeMachine
+{
+	public static function firstDayOfWeek($date = null)
+  {
+    if ($date instanceof \DateTime) {
+        $date = clone $date;
+    } else if (!$date) {
+        $date = new \DateTime();
+    } else {
+        $date = new \DateTime($date);
+    }
+
+    if ($date->format('N') != 1) {
+      $date->modify('last monday');
+    }
+    $date->setTime(0, 0, 0);
+    return $date;
+  }
+
+  public static function lastDayOfWeek($date = null)
+  {
+    if ($date instanceof \DateTime) {
+        $date = clone $date;
+    } else if (!$date) {
+        $date = new \DateTime();
+    } else {
+        $date = new \DateTime($date);
+    }
+    
+    if ($date->format('N') != 7) {
+      $date->modify('Next Sunday');  
+    } 
+    $date->setTime(23,59,59);
+    return $date;
+  }
+
+  public static function firstDayOfMonth($date = null)
+  {
+    if ($date instanceof \DateTime) {
+        $date = clone $date;
+    } else if (!$date) {
+        $date = new \DateTime();
+    } else {
+        $date = new \DateTime($date);
+    }
+
+    if ($date->format('d') != 1) {
+      $date->modify('first day of this month');
+    }
+    $date->setTime(0, 0, 0);
+    return $date;
+  }
+
+  public static function lastDayOfMonth($date = null)
+  {
+    if ($date instanceof \DateTime) {
+          $date = clone $date;
+      } else if (!$date) {
+          $date = new \DateTime();
+      } else {
+          $date = new \DateTime($date);
+      }
+           
+      $date->modify('last day of this month');  
+      
+      $date->setTime(23,59,59);
+      return $date;
+  }
+
+  public static function firstDayOfYear($date = null)
+  {
+      if ($date instanceof \DateTime) {
+          $date = clone $date;
+      } else if (!$date) {
+          $date = new \DateTime();
+      } else {
+          $date = new \DateTime($date);
+      }
+
+      
+      $date->modify('first day of January');
+      
+      $date->setTime(0, 0, 0);
+      return $date;
+  }
+  
+  public static function lastDayOfYear($date = null)
+  {
+    if ($date instanceof \DateTime) {
+      $date = clone $date;
+    } else if (!$date) {
+      $date = new \DateTime();
+    } else {
+      $date = new \DateTime($date);
+    }
+         
+    $date->modify('last day of December');  
+    
+    $date->setTime(23,59,59);
+    return $date;
+  }
+
+  public static function setTimeToZero($date)
+  {
+    $d = clone $date;
+    $d->setTime(00, 00, 00);
+    return $d;
+  }
+
+  public static function setTimeToMax($date)
+  {
+    $d = clone $date;
+    $d->setTime(23, 59, 59);
+    return $d;
+  }
+
+  public static function getDateTime($date = null, $timezone = null)
+  {
+  	if (in_array($timezone , timezone_identifiers_list())) {
+			$timezone = new \DateTimeZone($timezone);
+			echo "sí";
+		} else {
+			echo "NO";
+			$timezone = new \DateTimeZone(date_default_timezone_get());
+		}
+
+  	if ($date instanceof \DateTime) {
+        $date = clone $date;
+    } else if (!$date) {
+        $date = new \DateTime('now', $timezone);
+    } else {
+        $date = new \DateTime($date, $timezone);
+    }
+    //$date->setTime(0, 0, 0);
+    return $date;
+  }
+}
